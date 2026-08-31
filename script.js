f7d968ad9041b36538a2c4cee24d1866d930af2a
hernanblanco035-web <hernanblanco035@gmail.com>
2026-08-31 05:54:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const products=[
- {name:"Auriculares Bluetooth Pro",cat:"Audio",price:"$39.999",rating:"4,7",icon:"🎧",desc:"Buena batería, conexión inalámbrica y micrófono integrado.",url:"https://meli.la/18KeDbr"},
+ {name:"Auriculares Xiaomi Redmi Buds 6 Play Bluetooth",cat:"Audio",price:"$33.000",rating:"4,7",icon:"🎧",desc:"Auriculares Bluetooth Xiaomi compactos y prácticos para disfrutar música y llamadas.",url:"https://meli.la/18KeDbr"},
  {name:"Celular Samsung Galaxy A16 128GB",cat:"Tecnología",price:"$304.000",rating:"4,6",icon:"📱",desc:"Celular Samsung Galaxy A16 de 128GB para uso diario, redes y multimedia.",url:"https://meli.la/1g28Y8c"},
  {name:"Mouse Gaming RGB",cat:"Gaming",price:"$24.999",rating:"4,8",icon:"🖱️",desc:"Sensor preciso y diseño pensado para sesiones largas.",url:"TU_ENLACE_DE_AFILIADO_3"},
  {name:"Teclado mecánico compacto",cat:"Gaming",price:"$49.999",rating:"4,7",icon:"⌨️",desc:"Formato compacto, switches mecánicos y retroiluminación.",url:"TU_ENLACE_DE_AFILIADO_4"},
